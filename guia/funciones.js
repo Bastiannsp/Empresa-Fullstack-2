3c8756dc29d113aa28f5184c89c82c7443a170f4
mkben <xbenjamin629@gmail.com>
2025-08-29 09:44:26 -0400

    guia/29/06

--- guia/funciones.js
+++ guia/funciones.js
@@ -1,4 +1,5 @@
 function validarFormulario() {
+    console.log("Validando formulario")
     let nombre = document.getElementById("nombre").value.trim();
     let correo = document.getElementById("correo").value.trim();
     let mensaje = document.getElementById("mensaje").value.trim();
